ba85dbf529221a6e70366724fbb35d1947658c85
Mohamed-Magdiii <mm88mm05@yahoo.com>
2024-12-11 22:41:52 +0400

    fix-margin

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header: React.FC = React.memo(() => {
   return (
-    <header className="bg-gray-500 text-center text-white p-4">
+    <header className="bg-black text-center text-white p-4">
       <h1 className="text-2xl font-bold">IO-Tech</h1>
       <p>Electronics & Additive Manufacturing
         ...Materially Different
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,32 @@
+import React from "react";
 import PostList from "../Modules/Posts/PostList";
 import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const HomePage: React.FC = () => {
   const navigate = useNavigate();
 
   const handleCreatePost = () => {
-    navigate("/add-post"); 
+    navigate("/add-post");
   };
 
   return (
-    <div className="bg-gray-50 px-4">
+    <div className="bg-gray-50 flex flex-col">
       <Header />
-      <div className="py-8 ">
-  <div className="flex justify-end mb-6">
-    <button
-      onClick={handleCreatePost}
-      className="px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-lg shadow-md transition duration-200"
-    >
-      Create New Post
-    </button>
-  </div>
+      <div className="pt-4 px-4 pb-4">
+        <div className="flex justify-end mb-3">
+          <button
+            onClick={handleCreatePost}
+            className="px-4 py-2 text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 rounded-lg shadow-md transition duration-200"
+          >
+            Create New Post
+          </button>
+        </div>
 
-  <PostList />
-</div>
-      
+        <PostList />
+      </div>
+      <Footer/>
     </div>
   );
 };
